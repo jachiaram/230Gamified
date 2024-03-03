589cd3e1e0f520ddb6af8f76b35800e24b197efa
giopdev <giovanni.p.paladino@gmail.com>
2024-03-03 02:35:15 -0700

    machinetomips in main menu

--- Python_Files/main.py
+++ Python_Files/main.py
@@ -2,6 +2,7 @@ import os
 # import problems.multiply_algorithm as multiply_algorithm
 # import problems.division_algorithm as division_algorithm
 import utils.quiz_prompter as quiz
+import problems.machine_to_mips as machine_to_mips
 
 
 # Determines whether system is unix or windows based,
@@ -31,6 +32,7 @@ def mainMenuLoop():
         print(title)
         print("<1> Multiply 4-bit")
         print("<2> Divide 4-bit")
+        print("<3> Mips To Machine Code 1-line")
         print("\n<q> Quit")
         userInput = input("enter choice:")
 
@@ -40,6 +42,8 @@ def mainMenuLoop():
         elif userInput == '2':
             # division_algorithm.divAlg(4, 4)
             quiz.gameLoop(2)
+        elif userInput == '3':
+            machine_to_mips.machToMips()
         else:
             clearTerminalWindow()
             continue
